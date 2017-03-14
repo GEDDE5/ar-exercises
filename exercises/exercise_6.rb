bd5b4e41694ef045a00c25c423e4a6d774ff19e7
GEDDE5 <geddes.3754@gmail.com>
2017-03-14 12:01:00 -0700

    Completes Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+
+  has_many :employees
+
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+  @store1.employees.create(first_name: "Donald", last_name: "T", hourly_rate: 60)
+  @store1.employees.create(first_name: "Trump", last_name: "D", hourly_rate: 60)
+
+  @store2.employees.create(first_name: "Justin", last_name: "T", hourly_rate: 60)
+  @store2.employees.create(first_name: "Trudeau", last_name: "J", hourly_rate: 60)
+  @store2.employees.create(first_name: "Christy", last_name: "C", hourly_rate: 60)
+
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
