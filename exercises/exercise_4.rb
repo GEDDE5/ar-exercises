6bcf3823e023a93b65e559f069f03fcd8c59a96c
GEDDE5 <geddes.3754@gmail.com>
2017-03-14 11:48:08 -0700

    Completes Exercise 4: Loading a subset of stores
    Completes Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,41 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+
+  Store.create(
+     name: "Surrey",
+     annual_revenue: 224000,
+     mens_apparel: false,
+     womens_apparel: true
+  )
+
+  Store.create(
+    name: "Whistler",
+    annual_revenue: 1900000,
+    mens_apparel: true,
+    womens_apparel: false
+  )
+
+  Store.create(
+    name: "Yaletown",
+    annual_revenue: 430000,
+    mens_apparel: true,
+    womens_apparel: true
+  )
+
+  @mens_stores = Store.where(mens_apparel: true)
+
+  puts "Men's clothing stores:"
+  @mens_stores.each do | store |
+    puts "Name: #{store.name} - Annual revenue: #{store.annual_revenue}"
+  end
+
+
+  puts "Women's clothing stores ($1 000 000 or less in annual sales):"
+  Store.where(womens_apparel: true).where("annual_revenue < 1000000").each do | store |
+    puts "Name: #{store.name} - Annual revenue: #{store.annual_revenue}"
+  end
+
+
+end
