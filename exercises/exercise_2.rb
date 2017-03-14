d640278897fdd8434600ebb52291d296bddd8349
GEDDE5 <geddes.3754@gmail.com>
2017-03-14 11:32:00 -0700

    Completes Exercise 2: Update the first store
    Completes Exercise 2: Update the first store
    
    Completes Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+
+  @store1 = Store.find(1)
+  @store2 = Store.find(2)
+
+  @store1.name = "Burnsaby"
+  @store1.save
+
+end
